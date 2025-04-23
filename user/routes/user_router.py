83a7a06532c20a5af4cfa09de3d79d4d21fbaf49
TenacityDev <98986694+Tenacity-Dev@users.noreply.github.com>
2025-04-23 22:44:14 +0200

    update backend to use api prefix and tags

--- user/routes/user_router.py
+++ user/routes/user_router.py
@@ -6,17 +6,20 @@ from core.database import get_db
 from user.schemas.user import UserSchema, UserCreate
 from user.services.user_service import get_users, create_user, get_user, delete_user
 
-user_router = APIRouter()
+user_router = APIRouter(
+    prefix='/users',
+    tags=['Users']
+)
 
 
-@user_router.get('/users', response_model=list[UserSchema])
+@user_router.get('/', response_model=list[UserSchema])
 def user_list(db: Session = Depends(get_db)):
     db_users = get_users(db)
 
     return db_users
 
 
-@user_router.get('/users/{user_id}', response_model=UserSchema)
+@user_router.get('/{user_id}', response_model=UserSchema)
 def user_detail(user_id: int, db: Session = Depends(get_db)):
     db_user = get_user(db, user_id)
     if db_user is None:
@@ -25,7 +28,7 @@ def user_detail(user_id: int, db: Session = Depends(get_db)):
     return db_user
 
 
-@user_router.delete('/users/{user_id}')
+@user_router.delete('/{user_id}')
 def user_delete(user_id: int, db: Session = Depends(get_db)):
     db_user = get_user(db, user_id)
     if db_user is None:
@@ -34,6 +37,6 @@ def user_delete(user_id: int, db: Session = Depends(get_db)):
     return {"message": "User deleted"}
 
 
-@user_router.post("/users", response_model=UserSchema)
+@user_router.post("/", response_model=UserSchema)
 def user_post(user: UserCreate, db:Session = Depends(get_db)):
     return create_user(db, user)
